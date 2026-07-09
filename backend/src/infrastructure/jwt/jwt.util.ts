44e331fb09ed1cea611709d5cb8816e20426ba68
blackbox24 <nicardid@gmail.com>
2026-07-09 21:09:10 +0000

    fix: restructure JWT key loading logic for better clarity in development and production environments

--- backend/src/infrastructure/jwt/jwt.util.ts
+++ backend/src/infrastructure/jwt/jwt.util.ts
@@ -9,18 +9,25 @@ import type {
 } from "../../common/types/express.js";
 
 
+const node_env = process.env.NODE_ENV
+
 let privateKey = "";
 let publicKey = "";
 
 
-privateKey = fs.readFileSync(
-  process.env.JWT_PRIVATE_KEY_PATH || "./keys/private.pem",
-  "utf8",
-);
-publicKey = fs.readFileSync(
-  process.env.JWT_PUBLIC_KEY_PATH || "./keys/public.pem",
-  "utf8",
-);
+if(node_env === "development"){
+  privateKey = process.env.JWT_PRIVATE_KEY_PATH || "./keys/private.pem"
+  publicKey =process.env.JWT_PUBLIC_KEY_PATH || "./keys/public.pem"
+}else{
+  privateKey = fs.readFileSync(
+    process.env.JWT_PRIVATE_KEY_PATH || "./keys/private.pem",
+    "utf8",
+  );
+  publicKey = fs.readFileSync(
+    process.env.JWT_PUBLIC_KEY_PATH || "./keys/public.pem",
+    "utf8",
+  );
+}
 
 const ALGORITHM = "RS256";
 
